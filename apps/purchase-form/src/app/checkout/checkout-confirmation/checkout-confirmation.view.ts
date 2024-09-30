23988dca18b5c1cc443aac7bb48c12a64be2da56
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-09-30 16:08:57 +0200

    feat(confirmation): add confetti 🎊

--- apps/purchase-form/src/app/checkout/checkout-confirmation/checkout-confirmation.view.ts
+++ apps/purchase-form/src/app/checkout/checkout-confirmation/checkout-confirmation.view.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core'
+import { afterNextRender, Component } from '@angular/core'
 import { ButtonModule } from 'primeng/button'
 import { RouterLink } from '@angular/router'
+import confetti from 'canvas-confetti'
 
 @Component({
   selector: 'tz-checkout-confirmation',
@@ -13,4 +14,45 @@ import { RouterLink } from '@angular/router'
   `,
   imports: [ButtonModule, RouterLink]
 })
-export default class CheckoutConfirmationView {}
+export default class CheckoutConfirmationView {
+  constructor() {
+    afterNextRender(() => {
+      this.#fireConfetti(0.25, {
+        spread: 26,
+        startVelocity: 55
+      })
+      this.#fireConfetti(0.2, {
+        spread: 60
+      })
+      this.#fireConfetti(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+      })
+      this.#fireConfetti(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+      })
+      this.#fireConfetti(0.1, {
+        spread: 120,
+        startVelocity: 45
+      })
+    })
+  }
+
+  #fireConfetti(
+    particleRatio: number,
+    config: Partial<{ spread: number; startVelocity: number; decay: number; scalar: number }>
+  ) {
+    const count = 200
+    const defaults = { origin: { y: 0.7 } }
+
+    confetti(
+      Object.assign({}, defaults, config, {
+        particleCount: Math.floor(count * particleRatio)
+      })
+    )
+  }
+}
