04546334cf755258c73158de4f811eeeeef9fe9d
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-10-03 11:26:00 +0200

    feat(register): setup form

--- apps/purchase-form/src/app/app.component.ts
+++ apps/purchase-form/src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { PrimeNGConfig } from 'primeng/api';
-import { Aura } from 'primeng/themes/aura';
+import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { PrimeNGConfig } from 'primeng/api'
+import { Aura } from 'primeng/themes/aura'
 
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'app-root',
+  selector: 'tz-root',
   template: `
     <header>
-      <h1 class="text-red-500">Toolazon</h1>
+      <h1 class="text-yellow-400 font-bold text-center text-2xl p-5">Toolazon</h1>
     </header>
 
     <router-outlet></router-outlet>
-  `,
+  `
 })
 export class AppComponent {
   constructor(private primengConfig: PrimeNGConfig) {
@@ -22,9 +22,9 @@ export class AppComponent {
       options: {
         cssLayer: {
           name: 'primeng',
-          order: 'tailwind-base, primeng, tailwind-utilities',
-        },
-      },
-    });
+          order: 'tailwind-base, primeng, tailwind-utilities'
+        }
+      }
+    })
   }
 }
--- apps/purchase-form/src/app/registration/registration.view.ts
+++ apps/purchase-form/src/app/registration/registration.view.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core'
 import { ButtonModule } from 'primeng/button'
 import { RouterLink } from '@angular/router'
+import { InputTextModule } from 'primeng/inputtext'
 
 @Component({
   selector: 'tz-registration',
   standalone: true,
   template: `
-    <p-button
-      label="Register"
-      routerLink="/shopping/list"
-    ></p-button>
+    <form class="grid p-4 gap-4">
+      <fieldset class="flex flex-col gap-2">
+        <label for="firstname">Firstname</label>
+        <input
+          pInputText
+          id="firstname"
+        />
+      </fieldset>
+
+      <fieldset class="flex flex-col gap-2">
+        <label for="lastname">Lastname</label>
+        <input
+          pInputText
+          id="lastname"
+        />
+      </fieldset>
+
+      <fieldset class="flex flex-col gap-2">
+        <label for="email">E-Mail</label>
+        <input
+          pInputText
+          id="email"
+        />
+      </fieldset>
+
+      <fieldset class="flex flex-col gap-2">
+        <label for="email_confirmed">Confirm E-Mail</label>
+        <input
+          pInputText
+          id="email_confirmed"
+        />
+      </fieldset>
+
+      <p-button
+        label="Register"
+        routerLink="/shopping/list"
+      ></p-button>
+    </form>
   `,
-  imports: [ButtonModule, RouterLink]
+  imports: [ButtonModule, RouterLink, InputTextModule]
 })
 export default class RegistrationView {}
