e9d2c4510dda63695991b40926fe5b370c7ecd7f
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-10-05 16:00:35 +0200

    test(registration): provide test cases

--- apps/toolazon/src/app/registration/registration.schema.spec.ts
+++ apps/toolazon/src/app/registration/registration.schema.spec.ts
@@ -5,24 +5,27 @@ const RegistrationSchema = objectAsync({
   firstname: pipe(string('Please enter your firstname.'), nonEmpty('Your name must not be empty.'))
 })
 
-describe('When the user types no firstname', () => {
-  it('yields an error', async () => {
-    const input = {}
+describe('When the user does not provide a firstname', () => {
+  it.each`
+    noFirstname  | issueMessage
+    ${null}      | ${'Please enter your firstname.'}
+    ${undefined} | ${'Please enter your firstname.'}
+    ${''}        | ${'Your name must not be empty.'}
+  `(
+    'yields failure "$issueMessage" when given "$noFirstname"',
+    async ({
+      issueMessage,
+      noFirstname
+    }: {
+      noFirstname: null | undefined | ''
+      issueMessage: string
+    }) => {
+      const formValue = { firstname: noFirstname }
 
-    const result = await safeParseAsync(RegistrationSchema, input)
+      const result = await safeParseAsync(RegistrationSchema, formValue)
 
-    expect(result.success).toBeFalsy()
-    expect(result.issues?.[0].message).toBe('Please enter your firstname.')
-  })
-})
-
-describe('When the user types an empty firstname', () => {
-  it('yields an error', async () => {
-    const input = { firstname: '' }
-
-    const result = await safeParseAsync(RegistrationSchema, input)
-
-    expect(result.success).toBeFalsy()
-    expect(result.issues?.[0].message).toBe('Your name must not be empty.')
-  })
+      expect(result.success).toBeFalsy()
+      expect(result.issues?.[0].message).toBe(issueMessage)
+    }
+  )
 })
