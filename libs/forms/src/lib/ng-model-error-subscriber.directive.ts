bc1a78e92865240535f14331f3f377fb273089e0
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-10-08 11:00:54 +0200

    ref(ngModel): simplify code

--- libs/forms/src/lib/ng-model-error-subscriber.directive.ts
+++ libs/forms/src/lib/ng-model-error-subscriber.directive.ts
@@ -1,64 +1,42 @@
-import { AfterViewInit, DestroyRef, Directive, inject, input } from '@angular/core'
+import { afterNextRender, DestroyRef, Directive, inject, input } from '@angular/core'
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { NgControl } from '@angular/forms'
-import { tap } from 'rxjs'
+import { filter, map, tap } from 'rxjs'
 import { FormSchemaDirective } from './form-schema.directive'
 import { getControlPath } from './get-control-path'
-import { keyof, LooseObjectSchema } from 'valibot'
 
 @Directive({
   selector: '[ngModel]',
   standalone: true
 })
-export class NgModelErrorSubscriberDirective implements AfterViewInit {
+export class NgModelErrorSubscriberDirective {
   readonly #destroyRef = inject(DestroyRef)
   readonly #ngControl = inject(NgControl)
   readonly #formSchema = inject(FormSchemaDirective)
 
   name = input.required<NgControl['name']>()
 
-  ngAfterViewInit(): void {
-    this.#bindFormSettingErrors().pipe(takeUntilDestroyed(this.#destroyRef)).subscribe()
+  constructor() {
+    afterNextRender(() => this.#listenForControlErrors())
   }
 
-  #bindFormSettingErrors() {
-    return this.#formSchema.schemaIssues$.pipe(
-      tap(errors => {
-        const controlPath = getControlPath(
-          this.#formSchema.ngForm.control,
-          this.#ngControl.name?.toString() || '',
-          this.#ngControl.control
-        )
+  #listenForControlErrors() {
+    return this.#formSchema.schemaIssues$
+      .pipe(
+        map(errors => ({ errors, controlPath: this.#buildControlPath() })),
+        map(({ errors, controlPath }) => errors?.[controlPath] ?? null),
+        filter(maybeError => !!maybeError),
+        tap(error => this.#ngControl.control?.setErrors(error)),
+        takeUntilDestroyed(this.#destroyRef)
+      )
+      .subscribe()
+  }
 
-        const error = errors?.[controlPath] ?? null
-
-        if (error) this.#ngControl.control?.setErrors(error)
-      })
+  #buildControlPath() {
+    return getControlPath(
+      this.#formSchema.ngForm.control,
+      this.#ngControl.name?.toString() || '',
+      this.#ngControl.control
     )
-  }
-
-  #isControlPartOfTheSchema(
-    controlPathSegments: string[],
-    schema: LooseObjectSchema<any, any>
-  ): boolean {
-    const [key, ...rest] = controlPathSegments
-
-    try {
-      const schemaPicklist = keyof(schema as any)
-      const keys: string[] = schemaPicklist.options
-
-      if (keys.includes(key) && rest.length === 0) {
-        return true
-      }
-
-      if (keys.includes(key)) {
-        return this.#isControlPartOfTheSchema(rest, schema.entries[key])
-      }
-
-      return false
-    } catch {
-      // valibots keyof throws when the schema contains a record, since the data structure defers.
-      return true
-    }
   }
 }
