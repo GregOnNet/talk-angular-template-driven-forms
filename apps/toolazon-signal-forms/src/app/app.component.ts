fdb41677abd4a32234bada2e8d0881104cfb568c
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-09-24 13:36:10 +0200

    ref: remove unused code

--- apps/toolazon-signal-forms/src/app/app.component.ts
+++ apps/toolazon-signal-forms/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core'
-import { RouterModule } from '@angular/router'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
